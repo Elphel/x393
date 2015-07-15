3946f6c4f75cbf0929576a0e314f15db0f46a66c
Andrey Filippov <andrey@elphel.com>
2015-07-14 23:59:40 -0600

    Reverted changes - it was what I meant

--- axi/axibram_read.v
+++ axi/axibram_read.v
@@ -245,7 +245,7 @@ fifo_same_clock   #( .DATA_WIDTH(ADDRESS_BITS+20),.DATA_DEPTH(4))
         .sync_rst(1'b0), // input
         .we(arvalid && arready),
         .re(start_read_burst_w),
-        .data_in({arid[11:0], arburst[1:0],arsize[1:0],arlen[3:0],2'b00,araddr[ADDRESS_BITS-1:2]/*,araddr[ADDRESS_BITS+1:2]*/}),
+        .data_in({arid[11:0], arburst[1:0],arsize[1:0],arlen[3:0],araddr[ADDRESS_BITS+1:2]}), 
         .data_out({arid_out[11:0], arburst_out[1:0],arsize_out[1:0],arlen_out[3:0],araddr_out[ADDRESS_BITS-1:0]}), //SuppressThisWarning ISExst Assignment to arsize ignored, since the identifier is never used
         .nempty(ar_nempty),
         .half_full(ar_half_full)
