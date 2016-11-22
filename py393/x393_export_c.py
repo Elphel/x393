5125b8979cdf5859f19ed5c0e09968530b0786c2
AndreyFilippov <andrey@elphel.com>
2016-11-22 10:20:14 -0700

    made trigger-realted parameters to run through the sequencer too, continued

--- py393/x393_export_c.py
+++ py393/x393_export_c.py
@@ -1065,15 +1065,15 @@ class X393ExportC(object):
         c =  "sens_chn"
         sdefines +=[
             (('CAMSYNC control',)),
-            (("X393_CAMSYNC_MODE",                        "", vrlg.CAMSYNC_MODE +                    ba, 0, z3,   "x393_camsync_mode", "wo",        "CAMSYNC mode")),
-            (("X393_CAMSYNC_TRIG_SRC",                    "", vrlg.CAMSYNC_TRIG_SRC +                ba, 0, z3,   "x393_camsync_io", "wo",          "CAMSYNC trigger source")),
-            (("X393_CAMSYNC_TRIG_DST",                    "", vrlg.CAMSYNC_TRIG_DST +                ba, 0, z3,   "x393_camsync_io", "wo",          "CAMSYNC trigger destination")),
+            (("X393_CAMSYNC_MODE",                        c, vrlg.CAMSYNC_MODE +                     ba, 0, z3,   "x393_camsync_mode", "wo",        "CAMSYNC mode")),
+            (("X393_CAMSYNC_TRIG_SRC",                    c, vrlg.CAMSYNC_TRIG_SRC +                 ba, 0, z3,   "x393_camsync_io",   "wo",        "CAMSYNC trigger source")),
+            (("X393_CAMSYNC_TRIG_DST",                    c, vrlg.CAMSYNC_TRIG_DST +                 ba, 0, z3,   "x393_camsync_io",   "wo",        "CAMSYNC trigger destination")),
             (('_Trigger period has special value for small (<255) values written to this register',)),
             (('_    d == 0 - disable (stop periodic mode)',)),
             (('_    d == 1 - single trigger',)),
             (('_    d == 2..255 - set output pulse / input-output serial bit duration (no start generated)',)),
             (('_    d >= 256 - repetitive trigger',)),
-            (("X393_CAMSYNC_TRIG_PERIOD",                 "", vrlg.CAMSYNC_TRIG_PERIOD +             ba, 0, z3,   "u32*", "rw",                     "CAMSYNC trigger period")),
+            (("X393_CAMSYNC_TRIG_PERIOD",                 c,  vrlg.CAMSYNC_TRIG_PERIOD +             ba, 0, z3,   "u32*", "rw",                     "CAMSYNC trigger period")),
             (("X393_CAMSYNC_TRIG_DELAY",                  c,  vrlg.CAMSYNC_TRIG_DELAY0 +             ba, 1, z3,   "u32*", "rw",                     "CAMSYNC trigger delay"))]
         
         ba = vrlg.CMDFRAMESEQ_ADDR_BASE
@@ -1245,7 +1245,8 @@ class X393ExportC(object):
                 #processing sequencer command (have "w" and var_name and var_range = 0..3
                 #TODO: Add special character to rw meaning channel applicable
                 channelCmd=False
-                if var_name and address_inc and ('w' in rw) and var_range:
+#                if var_name and address_inc and ('w' in rw) and var_range:
+                if var_name and ('w' in rw) and var_range:
                     multivar = isinstance(address_inc,(list,tuple))
                     if multivar:
                         if isinstance(var_range[0],(list,tuple)) and (var_range[0][0] == 0) and (var_range[0][1] == 3):
